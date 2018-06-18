ac84652ceb82462d4f78eb49ef6eb17d9829eed0
Toni Lamont <tlam212@gmail.com>
2018-06-18 21:16:47 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+def fizzbuzz (int)
+  if int % 3 == 0 
+  return "Fizz"
+  
+  
+  elsif int % 5 == 0 
+  return "Buzz"
+  
+  
+  elsif int % 5 == 0 && int % 3 == 0 
+  return "FizzBuzz"
+  
+else 
+  return nil
+
+  end 
+  
+end
